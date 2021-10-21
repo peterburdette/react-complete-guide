c7864fd16ca6ceb57f7bfe513222f77b5d0e2e9a
Peter Burdette <peterburdette@gmail.com>
2021-10-21 17:09:33 -0400

    added css and structure to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+  return (
+    <div className="expense-item">
+      <div>March 28, 2021</div>
+      <div class="expense-item__description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">$294.67</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
