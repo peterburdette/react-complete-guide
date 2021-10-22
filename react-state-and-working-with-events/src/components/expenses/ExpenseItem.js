ea30787e09a4f1fbd8f2aa97407b4286b037f4cf
Peter Burdette <peterburdette@gmail.com>
2021-10-22 16:11:24 -0400

    added onClick event

--- react-state-and-working-with-events/src/components/expenses/ExpenseItem.js
+++ react-state-and-working-with-events/src/components/expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import Card from "../ui/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const handleClick = () => {
+    console.log('action!!!');
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expenseDate} />
@@ -10,6 +14,7 @@ const ExpenseItem = (props) => {
         <h2>{props.expenseTitle}</h2>
       </div>
       <div className="expense-item__price">${props.expenseAmount}</div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 }
