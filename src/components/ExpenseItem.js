611505a02ceca6d9fabf1ba0fde732b834da3cbf
Peter Burdette <peterburdette@gmail.com>
2021-10-22 15:12:01 -0400

    Added (children) props and introduced composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.expenseDate} />
       <div className="expense-item__description">
         <h2>{props.expenseTitle}</h2>
       </div>
       <div className="expense-item__price">${props.expenseAmount}</div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import './Expenses.css';
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <h2>Let's get started!</h2>
       {props.data.map((expense) => (
         <ExpenseItem
@@ -13,7 +14,7 @@ function Expenses(props) {
           expenseAmount={expense.amount}
         />
       ))}
-    </div>
+    </Card>
   );
 }
 
