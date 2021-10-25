9f2ebfdc8945fca08525b4057f13cb948731c614
Peter Burdette <peterburdette@gmail.com>
2021-10-25 16:14:30 -0400

    added two-way binding on form submission to reset fields

--- react-state-and-working-with-events/src/components/newExpense/ExpenseForm.js
+++ react-state-and-working-with-events/src/components/newExpense/ExpenseForm.js
@@ -1,80 +1,102 @@
-import React, { useState } from 'react';
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    // Uses multiple states to store single values
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+  // Uses multiple states to store single values
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-    // Uses a single state obj to store different values
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enteredAmount: '',
-    //     enteredDate: ''
-    // })
+  // Uses a single state obj to store different values
+  // const [userInput, setUserInput] = useState({
+  //     enteredTitle: '',
+  //     enteredAmount: '',
+  //     enteredDate: ''
+  // })
 
-    const titleChangeHandler = (event) => {
-        // calls setEnteredTitle function and updates title
-        setEnteredTitle(event.target.value);
+  const titleChangeHandler = (event) => {
+    // calls setEnteredTitle function and updates title
+    setEnteredTitle(event.target.value);
 
-        // prevState is a snapshot of the current state and will always give the latest up to date state
-        // setUserInput((prevState) => {
-        //     return { ...prevState, enteredTitle: event.target.value };
-        // });
-    };
-    
-    const amountChangeHandler = (event) => {
-        // calls setEnteredAmount function and updates amount
-        setEnteredAmount(event.target.value);
+    // prevState is a snapshot of the current state and will always give the latest up to date state
+    // setUserInput((prevState) => {
+    //     return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
 
-        // prevState is a snapshot of the current state and will always give the latest up to date state
-        // setUserInput((prevState) => {
-        //     return { ...prevState, enteredAmount: event.target.value };
-        // });
-    };
+  const amountChangeHandler = (event) => {
+    // calls setEnteredAmount function and updates amount
+    setEnteredAmount(event.target.value);
 
-    const dateChangeHandler = (event) => {
-        // calls setEnteredDate function and updates date
-        setEnteredDate(event.target.value);
+    // prevState is a snapshot of the current state and will always give the latest up to date state
+    // setUserInput((prevState) => {
+    //     return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
 
-        // prevState is a snapshot of the current state and will always give the latest up to date state
-        // setUserInput((prevState) => {
-        //     return { ...prevState, enteredDate: event.target.value };
-        // });
-    };
+  const dateChangeHandler = (event) => {
+    // calls setEnteredDate function and updates date
+    setEnteredDate(event.target.value);
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
+    // prevState is a snapshot of the current state and will always give the latest up to date state
+    // setUserInput((prevState) => {
+    //     return { ...prevState, enteredDate: event.target.value };
+    // });
+  };
+
+  const submitHandler = (event) => {  
+      const expenseData = {
+          title: enteredTitle,
+          amount: enteredAmount,
+          date: new Date(enteredDate),
         };
-        console.log(expenseData);
-    }
+        
+    event.preventDefault();
+    console.log(expenseData);
 
-    return(
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
-                </div>
-                <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
-                </div>
-                <div className="new-expense__control">
-                    <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
-            </div>
-        </form>
-    )
+    // resetting the state of the form fields to their inital values
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            value={enteredAmount}
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={enteredDate}
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
