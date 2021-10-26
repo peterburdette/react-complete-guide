c4bf205c12523e21062935573d07c9083ef7a113
Peter Burdette <peterburdette@gmail.com>
2021-10-26 09:32:31 -0400

    started using "Stateful Lists" and added funx to add an item to the list

--- react-state-and-working-with-events/src/App.js
+++ react-state-and-working-with-events/src/App.js
@@ -1,46 +1,51 @@
-import React from 'react';
-import NewExpense from './components/newExpense/NewExpense';
+import React, { useState } from "react";
+import NewExpense from "./components/newExpense/NewExpense";
 import Expenses from "./components/expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+    {
+        id: 1,
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date(2021, 2, 24),
+    },
+    {
+        id: 2,
+        title: "Mortgage Insurance",
+        amount: 427.97,
+        date: new Date(2021, 3, 25),
+    },
+    {
+        id: 3,
+        title: "Home Insurance",
+        amount: 199.99,
+        date: new Date(2021, 4, 26),
+    },
+    {
+        id: 4,
+        title: "Flood Insurance",
+        amount: 200.44,
+        date: new Date(2021, 5, 27),
+    },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 1,
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 24),
-    },
-    {
-      id: 2,
-      title: "Mortgage Insurance",
-      amount: 427.97,
-      date: new Date(2021, 3, 25),
-    },
-    {
-      id: 3,
-      title: "Home Insurance",
-      amount: 199.99,
-      date: new Date(2021, 4, 26),
-    },
-    {
-      id: 4,
-      title: "Flood Insurance",
-      amount: 200.44,
-      date: new Date(2021, 5, 27),
-    },
-  ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  // function to receive expenseData from NewExpense.js - as expenseData is being passed up it is also known as 'passing state up'
-  const addExpenseHandler = expenseData => {
-    console.log('In App.js', expenseData);
-  }
+    // function to receive expenseData from NewExpense.js - as expenseData is being passed up it is also known as 'passing state up'
+    const addExpenseHandler = (expenseData) => {
+        setExpenses((prevExpenses) => {
+            // prevExpenses is a snapshot of the current state and will always give the latest up to date state
+            return [expenseData, ...prevExpenses];
+        });
+    };
 
-  return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses data={expenses} />
-    </div>
-  );
-}
+    return (
+        <div>
+            <NewExpense onAddExpense={addExpenseHandler} />
+            <Expenses data={expenses} />
+        </div>
+    );
+};
 
 export default App;
