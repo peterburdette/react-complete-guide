f334a583bbfb49921f7b059b6b9f11649afde1e7
Peter Burdette <peterburdette@gmail.com>
2021-10-27 10:19:51 -0400

    added chart components to display data

--- react-state-and-working-with-events/src/components/expenses/Expenses.js
+++ react-state-and-working-with-events/src/components/expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpensesFilter from "./ExpensesFilter";
 import Card from "../ui/Card";
-import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2022");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
                 selected={filteredYear}
                 onChangeFilter={filterChangeHandler}
             />
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList items={filteredExpenses} />
         </Card>
     );
