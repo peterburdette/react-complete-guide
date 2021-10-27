02c79b3ca53bdbbfb73797c8a0e3c2ed01f00b15
Peter Burdette <peterburdette@gmail.com>
2021-10-27 14:29:55 -0400

    added css module so css is directly scoped with component

--- setting-css-classes-dynamically/src/components/UI/Button/Button.js
+++ setting-css-classes-dynamically/src/components/UI/Button/Button.js
@@ -1,38 +1,44 @@
-import styled from "styled-components";
+import React from "react";
+import styles from "./Button.module.css";
+// import styled from "styled-components";
 
 // 'button' is a method on the 'styled' object which will return an html button
-const Button = styled.button`
-    width: 100%;
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
+// const Button = styled.button`
+//     width: 100%;
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #8b005d;
+//     color: white;
+//     background: #8b005d;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//     cursor: pointer;
 
-    @media (min-width: 768px) {
-        width: auto;
-    }
+//     @media (min-width: 768px) {
+//         width: auto;
+//     }
 
-    &:focus {
-        outline: none;
-    }
+//     &:focus {
+//         outline: none;
+//     }
 
-    &:hover,
-    &:active {
-        background: #ac0e77;
-        border-color: #ac0e77;
-        box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-    }
-`; // the backticks indicate an 'attacked template literal'
+//     &:hover,
+//     &:active {
+//         background: #ac0e77;
+//         border-color: #ac0e77;
+//         box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//     }
+// `; // the backticks indicate an 'attacked template literal'
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = (props) => {
+    return (
+        <button
+            type={props.type}
+            className={styles.button} // styles returns an object with all the styling within the Button.module.css and you give it a target class
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    );
+};
 
 export default Button;
