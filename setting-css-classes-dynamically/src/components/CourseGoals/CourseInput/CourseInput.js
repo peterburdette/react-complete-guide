5c55b2103cf987c1d2db26763f99e932f2672e96
Peter Burdette <peterburdette@gmail.com>
2021-10-27 13:49:58 -0400

    added styles being passed as props inside of CourseInput.js

--- setting-css-classes-dynamically/src/components/CourseGoals/CourseInput/CourseInput.js
+++ setting-css-classes-dynamically/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,35 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+    & label {
+        font-weight: bold;
+        display: block;
+        margin-bottom: 0.5rem;
+        color: ${(props) => (props.invalid ? "red" : "black")};
+    }
+
+    & input {
+        display: block;
+        width: 100%;
+        border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+        background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+        font: inherit;
+        line-height: 1.5rem;
+        padding: 0 0.25rem;
+    }
+
+    & input:focus {
+        outline: none;
+        background: #fad0ec;
+        border-color: #8b005d;
+    }
+`;
 
 const CourseInput = (props) => {
     const [enteredValue, setEnteredValue] = useState("");
@@ -29,11 +57,11 @@ const CourseInput = (props) => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            {/* utilizes the 'isValid' state to dynamically add/remove css classes (inside CouresInput.css) */}
-            <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+            {/* passes the true/false state of 'isValid via prop to the css in the backticks */}
+            <FormControl invalid={!isValid}>
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler} />
-            </div>
+            </FormControl>
             <Button type="submit">Add Goal</Button>
         </form>
     );
