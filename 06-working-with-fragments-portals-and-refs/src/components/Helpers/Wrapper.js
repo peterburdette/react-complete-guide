a6f769df552ea494d46a02c1d6a739646b6d9a94
Peter Burdette <peterburdette@gmail.com>
2021-10-28 11:52:12 -0400

    created a wrapper component

---
+++ 06-working-with-fragments-portals-and-refs/src/components/Helpers/Wrapper.js
@@ -0,0 +1,6 @@
+// receives all contents as props and returns it -> this component helper acts like a wrapper
+const Wrapper = (props) => {
+    return props.children;
+};
+
+export default Wrapper;
