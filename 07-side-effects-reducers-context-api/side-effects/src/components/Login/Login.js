143f002599d6228262859694693f024e8cecf159
Peter Burdette <peterburdette@gmail.com>
2021-10-29 11:40:26 -0400

    organized projects

---
+++ 07-side-effects-reducers-context-api/side-effects/src/components/Login/Login.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+const Login = (props) => {
+    const [enteredEmail, setEnteredEmail] = useState("");
+    const [emailIsValid, setEmailIsValid] = useState();
+    const [enteredPassword, setEnteredPassword] = useState("");
+    const [passwordIsValid, setPasswordIsValid] = useState();
+    const [formIsValid, setFormIsValid] = useState(false);
+
+    useEffect(() => {
+        console.log("effect running");
+    });
+
+    // this will only re-run if 'enterEmail' or 'enterPassword' changes and nothing else changes after 500 milleseconds
+    useEffect(() => {
+        // side-effect function
+        const identifier = setTimeout(() => {
+            console.log("checking form validity");
+            setFormIsValid(
+                enteredEmail.includes("@") && enteredPassword.trim().length > 6
+            );
+        }, 500);
+
+        // this 'cleanup function' will run before 'useEffect' runs after 'useEffect' has run once
+        return () => {
+            console.log("cleanup");
+            // whenever the cleanup function runs the timer is reset so that when the next side-effect function runs we are able to set a new timer
+            // practically, this will allow the user to continuously type without activating the side-effect
+            // if there was an http request then it would only be sent once instead of x amount of times
+            clearTimeout(identifier);
+        };
+    }, [enteredEmail, enteredPassword]);
+
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value);
+    };
+
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value);
+    };
+
+    const validateEmailHandler = () => {
+        setEmailIsValid(enteredEmail.includes("@"));
+    };
+
+    const validatePasswordHandler = () => {
+        setPasswordIsValid(enteredPassword.trim().length > 6);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        props.onLogin(enteredEmail, enteredPassword);
+    };
+
+    return (
+        <Card className={classes.login}>
+            <form onSubmit={submitHandler}>
+                <div
+                    className={`${classes.control} ${
+                        emailIsValid === false ? classes.invalid : ""
+                    }`}
+                >
+                    <label htmlFor="email">E-Mail</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={enteredEmail}
+                        onChange={emailChangeHandler}
+                        onBlur={validateEmailHandler}
+                    />
+                </div>
+                <div
+                    className={`${classes.control} ${
+                        passwordIsValid === false ? classes.invalid : ""
+                    }`}
+                >
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={enteredPassword}
+                        onChange={passwordChangeHandler}
+                        onBlur={validatePasswordHandler}
+                    />
+                </div>
+                <div className={classes.actions}>
+                    <Button
+                        type="submit"
+                        className={classes.btn}
+                        disabled={!formIsValid}
+                    >
+                        Login
+                    </Button>
+                </div>
+            </form>
+        </Card>
+    );
+};
+
+export default Login;
