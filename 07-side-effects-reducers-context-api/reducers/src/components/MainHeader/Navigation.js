d73cdbc4a906f19ba7f0c7cf54dffc285113a09c
Peter Burdette <peterburdette@gmail.com>
2021-11-02 15:43:29 -0400

    utilized the useContext hook in place of <AuthContext.Consumer>

--- 07-side-effects-reducers-context-api/reducers/src/components/MainHeader/Navigation.js
+++ 07-side-effects-reducers-context-api/reducers/src/components/MainHeader/Navigation.js
@@ -1,40 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../context/auth-context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
-    return (
-        // Consumer is a React component that subscribes to context changes. Using this component lets you subscribe to a context within a function component.
-        <AuthContext.Consumer>
-            {/* Consumer always returns a function with a return statement where content is rendered */}
-            {(ctx) => {
-                return (
-                    <nav className={classes.nav}>
-                        <ul>
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <a href="/">Users</a>
-                                </li>
-                            )}
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <a href="/">Admin</a>
-                                </li>
-                            )}
-                            {ctx.isLoggedIn && (
-                                <li>
-                                    <button onClick={props.onLogout}>
-                                        Logout
-                                    </button>
-                                </li>
-                            )}
-                        </ul>
-                    </nav>
-                );
-            }}
-        </AuthContext.Consumer>
-    );
+    // using the useContext hook outputs the same functionalty as using <AuthContext.Consumer> - this way uses less code and is more elegant
+    const ctx = useContext(AuthContext);
+
+    return (ctx) => {
+        return (
+            <nav className={classes.nav}>
+                <ul>
+                    {ctx.isLoggedIn && (
+                        <li>
+                            <a href="/">Users</a>
+                        </li>
+                    )}
+                    {ctx.isLoggedIn && (
+                        <li>
+                            <a href="/">Admin</a>
+                        </li>
+                    )}
+                    {ctx.isLoggedIn && (
+                        <li>
+                            <button onClick={props.onLogout}>Logout</button>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        );
+    };
 };
 
 export default Navigation;
