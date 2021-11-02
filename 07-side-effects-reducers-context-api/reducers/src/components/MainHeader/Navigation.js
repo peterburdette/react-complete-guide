4d37e6e3545da9aee2db36396c864a4196c4907f
Peter Burdette <peterburdette@gmail.com>
2021-11-02 15:38:47 -0400

    added React context api so state can be passed to child components w/o using props

--- 07-side-effects-reducers-context-api/reducers/src/components/MainHeader/Navigation.js
+++ 07-side-effects-reducers-context-api/reducers/src/components/MainHeader/Navigation.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React from "react";
+import AuthContext from "../../context/auth-context";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+    return (
+        // Consumer is a React component that subscribes to context changes. Using this component lets you subscribe to a context within a function component.
+        <AuthContext.Consumer>
+            {/* Consumer always returns a function with a return statement where content is rendered */}
+            {(ctx) => {
+                return (
+                    <nav className={classes.nav}>
+                        <ul>
+                            {ctx.isLoggedIn && (
+                                <li>
+                                    <a href="/">Users</a>
+                                </li>
+                            )}
+                            {ctx.isLoggedIn && (
+                                <li>
+                                    <a href="/">Admin</a>
+                                </li>
+                            )}
+                            {ctx.isLoggedIn && (
+                                <li>
+                                    <button onClick={props.onLogout}>
+                                        Logout
+                                    </button>
+                                </li>
+                            )}
+                        </ul>
+                    </nav>
+                );
+            }}
+        </AuthContext.Consumer>
+    );
 };
 
 export default Navigation;
