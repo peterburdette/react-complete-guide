cc44bec5bb80839684c707eee1e32bcb69057099
Peter Burdette <peterburdette@gmail.com>
2021-11-02 15:47:45 -0400

    removed old code

--- 07-side-effects-reducers-context-api/reducers/src/components/MainHeader/Navigation.js
+++ 07-side-effects-reducers-context-api/reducers/src/components/MainHeader/Navigation.js
@@ -7,29 +7,27 @@ const Navigation = (props) => {
     // using the useContext hook outputs the same functionalty as using <AuthContext.Consumer> - this way uses less code and is more elegant
     const ctx = useContext(AuthContext);
 
-    return (ctx) => {
-        return (
-            <nav className={classes.nav}>
-                <ul>
-                    {ctx.isLoggedIn && (
-                        <li>
-                            <a href="/">Users</a>
-                        </li>
-                    )}
-                    {ctx.isLoggedIn && (
-                        <li>
-                            <a href="/">Admin</a>
-                        </li>
-                    )}
-                    {ctx.isLoggedIn && (
-                        <li>
-                            <button onClick={props.onLogout}>Logout</button>
-                        </li>
-                    )}
-                </ul>
-            </nav>
-        );
-    };
+    return (
+        <nav className={classes.nav}>
+            <ul>
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <a href="/">Admin</a>
+                    </li>
+                )}
+                {ctx.isLoggedIn && (
+                    <li>
+                        <button onClick={ctx.onLogout}>Logout</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    );
 };
 
 export default Navigation;
