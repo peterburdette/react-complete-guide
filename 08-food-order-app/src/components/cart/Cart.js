984578f07360c061046ec87f17eb346e03806a40
Peter Burdette <peterburdette@gmail.com>
2021-11-05 17:48:19 -0400

    afleshed out the shopping cart component

--- 08-food-order-app/src/components/cart/Cart.js
+++ 08-food-order-app/src/components/cart/Cart.js
@@ -1,8 +1,32 @@
 import React from "react";
-// import styles from '../cart/Cart.module.css';
+import styles from "../cart/Cart.module.css";
 
 const Cart = () => {
-    return <div></div>;
+    cartItems = (
+        <ul className={styles["cart-items"]}>
+            {[{ id: "1", name: "sushi", amount: 2, price: 12.99 }].map(
+                (item) => {
+                    <li>{item.name}</li>;
+                }
+            )}
+        </ul>
+    );
+
+    return (
+        <div>
+            {cartItems}
+            <div className={styles.total}>
+                <div>
+                    <span>Total Amount</span>
+                </div>
+                <div>35.95</div>
+            </div>
+            <div className={styles.actions}>
+                <button className={styles["button--alt"]}>Close</button>
+                <button className={styles.button}>Order</button>
+            </div>
+        </div>
+    );
 };
 
 export default Cart;
