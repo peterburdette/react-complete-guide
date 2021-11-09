0801aa33da68f79196a7c715af54fec6793dc92f
Peter Burdette <peterburdette@gmail.com>
2021-11-09 11:46:36 -0500

    closer look into child component re-evaluations

--- 09-react-optimization-techniques/src/App.js
+++ 09-react-optimization-techniques/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            {showParagraph && <p>This is new!</p>}
+            <DemoOutput show={showParagraph} />
             <Button
                 // type={}
                 // className={}
