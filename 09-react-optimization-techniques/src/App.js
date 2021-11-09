bae1675d1a4f4a1fdf12b887e61c96e8e2ce4a9a
Peter Burdette <peterburdette@gmail.com>
2021-11-09 14:10:43 -0500

    added dependency to the useCallback hook

--- 09-react-optimization-techniques/src/App.js
+++ 09-react-optimization-techniques/src/App.js
@@ -5,26 +5,27 @@ import "./App.css";
 
 function App() {
     const [showParagraph, setShowParagraph] = useState(false);
+    const [allowToggle, setAllowToggle] = useState(false);
 
     console.log("APP RUNNING");
 
     // Use Callback is a hook that allows us to basically store a function across component executions. So it allows us to tell React that we wanna save a function and that this function should not be recreated with every execution.
     const toggleParagraphHandler = useCallback(() => {
-        setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }, []);
+        if (allowToggle) {
+            setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+        }
+    }, [allowToggle]); // So here we would want to add allow toggle as a dependency in our dependency array. And that tells React that we generally want to store that function. But, whenever allow toggle changes and it has a new value, we want to recreate that function and store that new recreated function. And this ensures that we always use the latest allow toggle value inside of that stored function.
+
+    const allowToggleHandler = () => {
+        setAllowToggle(true);
+    };
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            <DemoOutput show={false} />
-            <Button
-                // type={}
-                // className={}
-                onClick={toggleParagraphHandler}
-                // disabled={}
-            >
-                Toggle Paragraph
-            </Button>
+            <DemoOutput show={showParagraph} />
+            <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+            <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
         </div>
     );
 }
