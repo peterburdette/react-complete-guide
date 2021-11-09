fc11ebcd5834bbe03f67c56bbc17fa8fd0120717
Peter Burdette <peterburdette@gmail.com>
2021-11-09 16:58:20 -0500

    componentized Users.js and used state and events

--- 10-react-class-based-components/src/components/User.js
+++ 10-react-class-based-components/src/components/User.js
@@ -6,3 +6,11 @@ export default class User extends Component {
         return <li className={classes.user}>{this.props.name}</li>;
     }
 }
+
+// import classes from './User.module.css';
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+// export default User;
