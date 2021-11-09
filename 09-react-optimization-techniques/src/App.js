c818b2056ae328c39c47ecc2b082d1d32d680a2b
Peter Burdette <peterburdette@gmail.com>
2021-11-09 13:58:05 -0500

    added useCallback hook

--- 09-react-optimization-techniques/src/App.js
+++ 09-react-optimization-techniques/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
@@ -8,14 +8,15 @@ function App() {
 
     console.log("APP RUNNING");
 
-    const toggleParagraphHandler = () => {
+    // Use Callback is a hook that allows us to basically store a function across component executions. So it allows us to tell React that we wanna save a function and that this function should not be recreated with every execution.
+    const toggleParagraphHandler = useCallback(() => {
         setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    };
+    }, []);
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            <DemoOutput show={showParagraph} />
+            <DemoOutput show={false} />
             <Button
                 // type={}
                 // className={}
--- 09-react-optimization-techniques/src/components/Demo/DemoOutput.js
+++ 09-react-optimization-techniques/src/components/Demo/DemoOutput.js
@@ -6,4 +6,5 @@ const DemoOutput = (props) => {
     return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 };
 
-export default DemoOutput;
+// memo tells React that for the 'DemoOutput' component it should look at the props that the component gets and check the new value for the props and compare it to a previous value for those props and only if the value of the prop changed the component should be re-executed and re-evaluated.
+export default React.memo(DemoOutput);
