722128f6dc6f0bf24d317d6bc78dfc6c7498bde9
Peter Burdette <peterburdette@gmail.com>
2021-11-09 16:17:46 -0500

    added useMemo hook

--- 09-react-optimization-techniques/src/App.js
+++ 09-react-optimization-techniques/src/App.js
@@ -1,13 +1,19 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
+import DemoList from "./components/Demo/DemoList";
 
 function App() {
+    const [listTitle, setListTitle] = useState("My List");
     const [showParagraph, setShowParagraph] = useState(false);
     const [allowToggle, setAllowToggle] = useState(false);
 
     console.log("APP RUNNING");
+
+    const changeTitleHandler = useCallback(() => {
+        setListTitle("New Title");
+    }, []);
 
     // Use Callback is a hook that allows us to basically store a function across component executions. So it allows us to tell React that we wanna save a function and that this function should not be recreated with every execution.
     const toggleParagraphHandler = useCallback(() => {
@@ -20,10 +26,14 @@ function App() {
         setAllowToggle(true);
     };
 
+    const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            <DemoOutput show={showParagraph} />
+            {/* <DemoOutput show={showParagraph} /> */}
+            <DemoList title={listTitle} items={listItems} />
+            <Button onClick={changeTitleHandler}>Change List Title</Button>
             <Button onClick={allowToggleHandler}>Allow Toggling</Button>
             <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
         </div>
---
+++ 09-react-optimization-techniques/src/components/Demo/DemoList.js
@@ -0,0 +1,25 @@
+import React, { useMemo } from "react";
+import classes from "./DemoList.module.css";
+
+const DemoList = (props) => {
+    const { items } = props;
+
+    const sortedList = useMemo(() => {
+        console.log("ITEMS SORTED");
+        return props.items.sort((a, b) => a - b);
+    }, [items]);
+    console.log("DEMOLIST IS RUNNING");
+
+    return (
+        <div className={classes.list}>
+            <h2>{props.title}</h2>
+            <ul>
+                {sortedList.map((item) => {
+                    return <li key={item}>{item}</li>;
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default React.memo(DemoList);
