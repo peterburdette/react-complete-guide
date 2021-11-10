9f1a3ebdd9bb657b1884c0819c0d46e0c83f85f6
Peter Burdette <peterburdette@gmail.com>
2021-11-10 16:28:41 -0500

    utilized async/await to fetch data

--- 11-sending-http-requests-connecting-to-a-database/src/App.js
+++ 11-sending-http-requests-connecting-to-a-database/src/App.js
@@ -6,30 +6,27 @@ import "./App.css";
 function App() {
     const [movies, setMovies] = useState([]);
 
-    function fetchMoviesHandler() {
-        // the 'response' is a json object with data that comes from the api. The 'response' will need to be converted to a javascript object so it can be used within the application
+    async function fetchMoviesHandler() {
+        // The 'response' is a json object with data that comes from the api. The 'response' will need to be converted to a javascript object so it can be used within the application
+
+        // Javascript promises are being utilized to fetch the data. When dealing with promises, you can build these 'then' chains here, so Then call after Then call, but you can also use an alternative syntax, async await. You can add the 'async' keyword in front of the function and then 'await' in front of the operation which is returning a promise.
 
         // fetching the json data from the api url and 'then' handling the response
-        fetch("https://swapi.dev/api/films/")
-            .then((response) => {
-                // 'response' data transformation step from json object to javascript object
-                return response.json();
-            })
-            // 'data' is the transformed data from the 'return response.json()' statement
-            .then((data) => {
-                // transform the data again so that the properties within the object match what we want them to be called
-                const transformedMovies = data.results.map((movieData) => {
-                    // mapping the object to new properties
-                    return {
-                        id: movieData.episode_id,
-                        title: movieData.title,
-                        openingText: movieData.opening_crawl,
-                        releaseDate: movieData.release_date,
-                    };
-                });
-                // sets the state of 'movies' to the api data
-                setMovies(transformedMovies);
-            });
+        const response = await fetch("https://swapi.dev/api/films/");
+        // transforms the response into json
+        const data = await response.json();
+        // transform the data again so that the properties within the object match what we want them to be called
+        const transformedMovies = data.results.map((movieData) => {
+            // maps the object to new properties
+            return {
+                id: movieData.episode_id,
+                title: movieData.title,
+                openingText: movieData.opening_crawl,
+                releaseDate: movieData.release_date,
+            };
+        });
+        // sets the state of 'movies' to the api data
+        setMovies(transformedMovies);
     }
 
     return (
