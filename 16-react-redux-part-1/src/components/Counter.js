f94403746962e461fb0d36b01b399c667f63832d
Peter Burdette <peterburdette@gmail.com>
2021-11-30 11:19:59 -0500

    added another redux state

--- 16-react-redux-part-1/src/components/Counter.js
+++ 16-react-redux-part-1/src/components/Counter.js
@@ -4,7 +4,8 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
     const dispatch = useDispatch();
-    const counter = useSelector((state) => state.counter);
+    const counter = useSelector((state) => state.counter); // looks at 'counter' property within redux state
+    const showCounter = useSelector((state) => state.showCounter); // looks at 'showCounter' property within redux state
 
     const increaseHandler = () => {
         dispatch({ type: "increase", amount: 5 });
@@ -18,12 +19,14 @@ const Counter = () => {
         dispatch({ type: "decrement" });
     };
 
-    const toggleCounterHandler = () => {};
+    const toggleCounterHandler = () => {
+        dispatch({ type: "toggle" });
+    };
 
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
-            <div className={classes.value}>{counter}</div>
+            {showCounter && <div className={classes.value}>{counter}</div>}
             <div>
                 <button onClick={incrementHandler}>Increment</button>
                 <button onClick={increaseHandler}>Increase by 5</button>
