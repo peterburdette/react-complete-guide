d69da1cf876b10ec482fd7b31beab72cd1b77809
Peter Burdette <peterburdette@gmail.com>
2021-12-02 15:42:50 -0500

    added a Layout wrapper component and navigation

--- 19-react-router-part-2/src/App.js
+++ 19-react-router-part-2/src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
+import Layout from "./components/layout/Layout";
 import QuoteDetail from "./pages/QuoteDetail";
 import AllQuotes from "./pages/AllQuotes";
 import AddNew from "./pages/AddNew";
 
 function App() {
     return (
-        <Switch>
-            <Route path="/" exact>
-                <Redirect to="/quotes" />
-            </Route>
-            <Route path="/quotes" exact>
-                <AllQuotes />
-            </Route>
-            <Route path="/quotes/:quoteId">
-                <QuoteDetail />
-            </Route>
-            <Route path="/add-new">
-                <AddNew />
-            </Route>
-        </Switch>
+        <Layout>
+            <Switch>
+                <Route path="/" exact>
+                    <Redirect to="/quotes" />
+                </Route>
+                <Route path="/quotes" exact>
+                    <AllQuotes />
+                </Route>
+                <Route path="/quotes/:quoteId">
+                    <QuoteDetail />
+                </Route>
+                <Route path="/add-new">
+                    <AddNew />
+                </Route>
+            </Switch>
+        </Layout>
     );
 }
 
