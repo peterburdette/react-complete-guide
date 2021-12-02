b3b663e5e5c623335245b2d5a94f3a4ae535ca5c
Peter Burdette <peterburdette@gmail.com>
2021-12-02 16:08:12 -0500

    implemented the QuoteDetails component and outputted data onto the page

--- 19-react-router-part-2/src/pages/QuoteDetail.js
+++ 19-react-router-part-2/src/pages/QuoteDetail.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+    {
+        id: "q1",
+        author: "Peter",
+        text: "Learning ReactJS is fun!",
+    },
+    {
+        id: "q2",
+        author: "Bob",
+        text: "The Lord of the Rings is great!",
+    },
+];
 
 const QuoteDetail = () => {
     const params = useParams();
 
+    const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+    if (!quote) {
+        return "No quote found.";
+    }
+
     return (
         <section>
-            <h1>Quote Detail</h1>
-            <p>{params.quoteId}</p>
+            <HighlightedQuote text={quote.text} author={quote.author} />
             <Route path="/quotes/:quoteId/comments">
                 <Comments />
             </Route>
