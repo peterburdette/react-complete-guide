dcd2c53813e172950e10a94b46901cc0da7780e2
Peter Burdette <peterburdette@gmail.com>
2021-12-02 15:57:54 -0500

    hooked up the Quote Details page with the Quotes page

--- 19-react-router-part-2/src/pages/AddNew.js
+++ 19-react-router-part-2/src/pages/AddNew.js
@@ -1,9 +1,15 @@
 import React from "react";
+import QuoteForm from "../components/quotes/QuoteForm";
 
 const AddNew = () => {
+    const addQuoteHandler = (quoteData) => {
+        console.log(quoteData);
+    };
+
     return (
         <section>
             <h1>Add New Quote</h1>
+            <QuoteForm onAddQuote={addQuoteHandler} />
         </section>
     );
 };
