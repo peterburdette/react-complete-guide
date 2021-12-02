be7c15028e3b4ac614c1fdcf2a5fe4a7a97d3525
Peter Burdette <peterburdette@gmail.com>
2021-12-02 16:33:31 -0500

    implemented react-router-dom programmatic navigation using "push/replace"

--- 19-react-router-part-2/src/pages/AddNew.js
+++ 19-react-router-part-2/src/pages/AddNew.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const AddNew = () => {
+    const history = useHistory();
+
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData);
+
+        history.push("/quotes");
     };
 
     return (
