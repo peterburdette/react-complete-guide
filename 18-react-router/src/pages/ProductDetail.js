093a9d685527cb650530fdea6aae43e948aab554
Peter Burdette <peterburdette@gmail.com>
2021-12-02 09:48:16 -0500

    extracted route params for use within the page using the custom hook "useParams" from react-router-dom

--- 18-react-router/src/pages/ProductDetail.js
+++ 18-react-router/src/pages/ProductDetail.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
+    const params = useParams();
+
+    console.log(params.productId);
+
     return (
         <section>
-            <h1>Product Detail</h1>
+            <h1>Product Detail - {params.productId}</h1>
         </section>
     );
 };
