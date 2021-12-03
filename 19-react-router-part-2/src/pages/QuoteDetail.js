921fbbce087f30cd3b0283563a9da101c68a167f
Peter Burdette <peterburdette@gmail.com>
2021-12-03 08:58:46 -0500

    utilized nested routes to conditionally show gui elements

--- 19-react-router-part-2/src/pages/QuoteDetail.js
+++ 19-react-router-part-2/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link, useHistory } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -18,6 +18,7 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
     const params = useParams();
+    const history = useHistory();
 
     const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -28,7 +29,19 @@ const QuoteDetail = () => {
     return (
         <section>
             <HighlightedQuote text={quote.text} author={quote.author} />
-            <Route path="/quotes/:quoteId/comments">
+
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className="centered">
+                    <Link
+                        className="btn--flat"
+                        to={`/quotes/${params.quoteId}/comments`}
+                    >
+                        Load Comments
+                    </Link>
+                </div>
+            </Route>
+
+            <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
         </section>
