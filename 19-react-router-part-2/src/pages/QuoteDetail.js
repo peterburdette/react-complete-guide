42fafe2c9724c2785d2e16dd59528a900cf37468
Peter Burdette <peterburdette@gmail.com>
2021-12-03 16:32:37 -0500

    adjusted path destinations to be more dynamic

--- 19-react-router-part-2/src/pages/QuoteDetail.js
+++ 19-react-router-part-2/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Route, Link, useHistory } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -17,8 +17,10 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+    const match = useRouteMatch();
     const params = useParams();
-    const history = useHistory();
+
+    console.log(match);
 
     const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -30,18 +32,15 @@ const QuoteDetail = () => {
         <section>
             <HighlightedQuote text={quote.text} author={quote.author} />
 
-            <Route path={`/quotes/${params.quoteId}`} exact>
+            <Route path={`${match.path}`} exact>
                 <div className="centered">
-                    <Link
-                        className="btn--flat"
-                        to={`/quotes/${params.quoteId}/comments`}
-                    >
+                    <Link className="btn--flat" to={`${match.url}/comments`}>
                         Load Comments
                     </Link>
                 </div>
             </Route>
 
-            <Route path={`/quotes/${params.quoteId}/comments`}>
+            <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
         </section>
