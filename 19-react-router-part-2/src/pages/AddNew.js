1dfdda5d2562909307f6d832684ebffb7e704886
Peter Burdette <peterburdette@gmail.com>
2021-12-06 14:33:44 -0500

    added functionality to POST to DB

--- 19-react-router-part-2/src/pages/AddNew.js
+++ 19-react-router-part-2/src/pages/AddNew.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
+
 const AddNew = () => {
+    const { sendRequest, status } = useHttp(addQuote);
     const history = useHistory();
+
+    useEffect(() => {
+        if (status === "completed") {
+            history.push("/quotes");
+        }
+    }, [status, history]);
 
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData);
-
-        history.push("/quotes");
+        // POSTs the quoteData using the function sendReqest from the use-http file
+        sendRequest(quoteData);
     };
 
     return (
         <section>
             <h1>Add New Quote</h1>
-            <QuoteForm onAddQuote={addQuoteHandler} />
+            <QuoteForm
+                isLoading={status === "pending"}
+                onAddQuote={addQuoteHandler}
+            />
         </section>
     );
 };
