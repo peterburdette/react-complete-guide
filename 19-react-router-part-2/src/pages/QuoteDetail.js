619aab8604172f4c3601807979928d841b5c78a8
Peter Burdette <peterburdette@gmail.com>
2021-12-06 15:00:47 -0500

    hooked up sendRequest functionality to pull in quote data to the QuoteDetails component

--- 19-react-router-part-2/src/pages/QuoteDetail.js
+++ 19-react-router-part-2/src/pages/QuoteDetail.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-    {
-        id: "q1",
-        author: "Peter",
-        text: "Learning ReactJS is fun!",
-    },
-    {
-        id: "q2",
-        author: "Bob",
-        text: "The Lord of the Rings is great!",
-    },
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuoteDetail = () => {
     const match = useRouteMatch();
     const params = useParams();
+    const { quoteId } = params; // object destructuring to pull the quoteId out of the params obj
 
-    console.log(match);
+    const {
+        sendRequest,
+        status,
+        data: loadedQuote,
+        error,
+    } = useHttp(getSingleQuote, true);
 
-    const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
 
-    if (!quote) {
-        return "No quote found.";
+    if (status === "pending") {
+        return (
+            <div className="centered">
+                <LoadingSpinner />;
+            </div>
+        );
+    }
+
+    if (status === "error") {
+        return <p className="centered">{error}</p>;
+    }
+
+    if (!loadedQuote.text) {
+        return <p>No quote found.</p>;
     }
 
     return (
         <section>
-            <HighlightedQuote text={quote.text} author={quote.author} />
+            <HighlightedQuote
+                text={loadedQuote.text}
+                author={loadedQuote.author}
+            />
 
             <Route path={`${match.path}`} exact>
                 <div className="centered">
