68fe14a120be1bb3089fdb37b523b72763e7ba92
Peter Burdette <peterburdette@gmail.com>
2021-12-07 11:29:54 -0500

    updated NavLink activeClassName implementation to utilize className via v6 standards

--- 20-react-router-version-6/src/components/MainHeader.js
+++ 20-react-router-version-6/src/components/MainHeader.js
@@ -1,26 +1,36 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import classes from './MainHeader.module.css';
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
-  return (
-    <header className={classes.header}>
-      <nav>
-        <ul>
-          <li>
-            <NavLink activeClassName={classes.active} to='/welcome'>
-              Welcome
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName={classes.active} to='/products'>
-              Products
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={classes.header}>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink
+                            className={(navData) =>
+                                navData.isActive ? classes.active : ""
+                            }
+                            to="/welcome"
+                        >
+                            Welcome
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={(navData) =>
+                                navData.isActive ? classes.active : ""
+                            }
+                            to="/products"
+                        >
+                            Products
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default MainHeader;
