d5b4ab7d7f2d020178e9dce5d64cd959ecbcc97d
Peter Burdette <peterburdette@gmail.com>
2021-12-07 12:14:02 -0500

    added notes on useNavigate

--- 21-react-router-version-6-part-2/src/pages/Products.js
+++ 21-react-router-version-6-part-2/src/pages/Products.js
@@ -1,22 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 const Products = () => {
-  return (
-    <section>
-      <h1>The Products Page</h1>
-      <ul>
-        <li>
-          <Link to='/products/p1'>A Book</Link>
-        </li>
-        <li>
-          <Link to='/products/p2'>A Carpet</Link>
-        </li>
-        <li>
-          <Link to='/products/p3'>An Online Course</Link>
-        </li>
-      </ul>
-    </section>
-  );
+    // navigate could be used within useEffect or within a handler
+    // const navigate = useNavigate();
+    // you can pass numbers like '1' to go forward or '-1' to go back
+    // navigate(-1);
+
+    // you can navigate progromatically by passing a string
+    // navigate('/welcome');
+
+    // you can redirect by passing a second argument as an object with some options
+    // navigate('/welcome', {replace: true});
+
+    return (
+        <section>
+            <h1>The Products Page</h1>
+            <ul>
+                <li>
+                    <Link to="/products/p1">A Book</Link>
+                </li>
+                <li>
+                    <Link to="/products/p2">A Carpet</Link>
+                </li>
+                <li>
+                    <Link to="/products/p3">An Online Course</Link>
+                </li>
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
