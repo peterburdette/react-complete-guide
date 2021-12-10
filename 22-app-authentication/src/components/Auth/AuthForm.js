30cea2eaa7b3d534d5286acf02b6c98df501e3e1
Peter Burdette <peterburdette@gmail.com>
2021-12-10 17:20:14 -0500

    implemented user signup using Firebase Auth REST API

--- 22-app-authentication/src/components/Auth/AuthForm.js
+++ 22-app-authentication/src/components/Auth/AuthForm.js
@@ -1,39 +1,87 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+    const [isLogin, setIsLogin] = useState(true);
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        if (isLogin) {
+        } else {
+            fetch(
+                "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDEtq9PrVnQaZUHuTYAIMpu-RS4Jjt7q90",
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: enteredEmail,
+                        password: enteredPassword,
+                        returnSecureTokenKey: true,
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            ).then((res) => {
+                if (res.ok) {
+                    // ...
+                } else {
+                    return res.json().then((data) => {
+                        //show error modal
+                        console.log(data);
+                    });
+                }
+            });
+        }
+    };
+
+    return (
+        <section className={classes.auth}>
+            <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+            <form onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor="email">Your Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        ref={emailInputRef}
+                        required
+                    />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="password">Your Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        ref={passwordInputRef}
+                        required
+                    />
+                </div>
+                <div className={classes.actions}>
+                    <button>{isLogin ? "Login" : "Create Account"}</button>
+                    <button
+                        type="button"
+                        className={classes.toggle}
+                        onClick={switchAuthModeHandler}
+                    >
+                        {isLogin
+                            ? "Create new account"
+                            : "Login with existing account"}
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
 };
 
 export default AuthForm;
