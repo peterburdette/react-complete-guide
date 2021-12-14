faa536937dca15ea9497ec2b839d4dc8accbdcf1
Peter Burdette <peterburdette@gmail.com>
2021-12-14 17:44:14 -0500

    implemented protected pages - utilized redirects and context

--- 22-app-authentication/src/App.js
+++ 22-app-authentication/src/App.js
@@ -1,26 +1,38 @@
-import { Switch, Route } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+    const authCtx = useContext(AuthContext);
+
+    const isLoggedIn = authCtx.isLoggedIn;
+
+    return (
+        <Layout>
+            <Switch>
+                <Route path="/" exact>
+                    <HomePage />
+                </Route>
+                {!isLoggedIn && (
+                    <Route path="/auth">
+                        <AuthPage />
+                    </Route>
+                )}
+                <Route path="/profile">
+                    {isLoggedIn && <UserProfile />}
+                    {!isLoggedIn && <Redirect to="/auth" />}
+                </Route>
+                <Route path="*">
+                    <Redirect to="/" />
+                </Route>
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
