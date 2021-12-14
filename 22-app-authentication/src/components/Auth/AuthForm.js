2b29216ef80463475a81af2cb7a2b3fe0067747d
Peter Burdette <peterburdette@gmail.com>
2021-12-14 11:51:08 -0500

    implemented an auth-context to manage the login state

--- 22-app-authentication/src/components/Auth/AuthForm.js
+++ 22-app-authentication/src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
@@ -8,6 +9,8 @@ const AuthForm = () => {
 
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
+
+    const authCtx = useContext(AuthContext);
 
     const switchAuthModeHandler = () => {
         setIsLogin((prevState) => !prevState);
@@ -61,6 +64,7 @@ const AuthForm = () => {
             })
             .then((data) => {
                 console.log(data);
+                authCtx.login(data.idToken);
             })
             .catch((err) => {
                 alert(err.message);
